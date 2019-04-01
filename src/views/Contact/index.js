cfa1da23029baa0095ecd70c334a446d64f6290f
vip1all <vip.all@hotmail.com>
2019-04-01 09:53:01 +0200

    Added navSetBackground to export from Nav/index.js

--- src/views/Contact/index.js
+++ src/views/Contact/index.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import ClinicBackground from "../../components/Containers/ClinicBackground";
+import { useNavSetBackground } from "../../components/Nav";
+import Info from "./Info";
+import Form from "./Form";
+
+const Wrapper = styled(ClinicBackground)`
+  min-height: 100vh;
+  padding-top: 10rem;
+`;
 
 const Contact = () => {
-  return <div>Contact</div>;
+  // disable nav background on this route
+  const setNavBackground = useNavSetBackground();
+  useEffect(() => {
+    setNavBackground(false);
+  }, [setNavBackground]);
+
+  return (
+    <Wrapper>
+      <Info />
+      <Form />
+    </Wrapper>
+  );
 };
 
 export default Contact;
