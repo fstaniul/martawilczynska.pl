eaa1f0b3e914194fbbd692a02d33fca8462a4793
vip1all <vip.all@hotmail.com>
2019-04-06 15:14:22 +0200

    Fixed eslint error

--- src/components/Buttons/JumpToTop.js
+++ src/components/Buttons/JumpToTop.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useCallback } from "react";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { colors } from "../../util/styles";
