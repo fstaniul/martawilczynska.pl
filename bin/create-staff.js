34b15ba2261fffc88aff992710bf28756f09bd0d
vip1all <vip.all@hotmail.com>
2019-04-08 14:06:37 +0200

    Updated create-staff script to sort images by their name

--- bin/create-staff.js
+++ bin/create-staff.js
@@ -1,11 +1,11 @@
-const sharp = require("sharp");
-const fs = require("fs");
-const path = require("path");
-const showdown = new (require("showdown")).Converter();
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
+const showdown = new (require('showdown')).Converter();
 
-const photosFolder = path.resolve(__dirname, "../public/static/media/staff-galery/image");
-const outputFolder = path.resolve(photosFolder, "../thumbnails");
-const staffGaleryJSON = [];
+const photosFolder = path.resolve(__dirname, '../public/static/media/staff-galery/image');
+const outputFolder = path.resolve(photosFolder, '../thumbnails');
+let staffGaleryJSON = [];
 let done = 0;
 
 fs.readdirSync(photosFolder).forEach((photo, index, photos) => {
@@ -16,29 +16,30 @@ fs.readdirSync(photosFolder).forEach((photo, index, photos) => {
     .resize(400, 400)
     .toFile(outputPath, err => {
       done++;
-      if (err) console.log("Failed to transform", photoPath, "\n", err);
+      if (err) console.log('Failed to transform', photoPath, '\n', err);
       else {
         staffGaleryJSON.push({
           image: `/static/media/staff-galery/image/${photo}`,
           thumbnail: `/static/media/staff-galery/thumbnails/${photo}`
         });
-        console.log("Successfully transformed", photoPath);
+        console.log('Successfully transformed', photoPath);
         if (done === photos.length) {
-          fs.writeFileSync(path.resolve(__dirname, "../src/assets/staff-galery.json"), JSON.stringify(staffGaleryJSON));
-          console.log("Successfully created staff-galery.json");
+          staffGaleryJSON = staffGaleryJSON.sort((x, y) => x.image.localeCompare(y.image));
+          fs.writeFileSync(path.resolve(__dirname, '../src/assets/staff-galery.json'), JSON.stringify(staffGaleryJSON));
+          console.log('Successfully created staff-galery.json');
         }
       }
     });
 });
 
-const staffContentFolder = path.resolve(__dirname, "../data/staff");
-const localeFilesFolder = path.resolve(__dirname, "../src/assets/locale");
+const staffContentFolder = path.resolve(__dirname, '../data/staff');
+const localeFilesFolder = path.resolve(__dirname, '../src/assets/locale');
 
 fs.readdirSync(staffContentFolder).forEach((staffContent, index, files) => {
-  const locale = staffContent.replace(".md", "");
-  const outputFile = path.join(localeFilesFolder, locale + ".json");
-  const mdContent = fs.readFileSync(path.join(staffContentFolder, staffContent), { encoding: "utf8" });
-  const localeFile = JSON.parse(fs.readFileSync(outputFile, { encoding: "utf8" }));
-  localeFile["staff.content"] = showdown.makeHtml(mdContent);
-  fs.writeFileSync(outputFile, JSON.stringify(localeFile, null, 2), { encoding: "utf8" });
+  const locale = staffContent.replace('.md', '');
+  const outputFile = path.join(localeFilesFolder, locale + '.json');
+  const mdContent = fs.readFileSync(path.join(staffContentFolder, staffContent), { encoding: 'utf8' });
+  const localeFile = JSON.parse(fs.readFileSync(outputFile, { encoding: 'utf8' }));
+  localeFile['staff.content'] = showdown.makeHtml(mdContent);
+  fs.writeFileSync(outputFile, JSON.stringify(localeFile, null, 2), { encoding: 'utf8' });
 });
