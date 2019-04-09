f253b747b1b4467d34c580917ca49cd48d1d8832
vip1all <vip.all@hotmail.com>
2019-04-09 21:03:56 +0200

    Added map with markers on it

---
+++ src/views/Contact/Form.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FormattedMessage } from 'react-intl';
+import { query } from '../../util/styles';
+import ContactForm from '../../components/Contact/ContactForm';
+import { SideBySideLadyImage } from '../../components/OpenEyesLadyImage';
+import SideBySide, { Left } from '../../components/Containers/SideBySide';
+import { SectionHeading, SubHeading } from '../../components/Headings';
+
+const FormContainer = styled(SideBySide)``;
+
+const FormWrapper = styled(Left)`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: stretch;
+
+  ${query.lg`
+    padding-left: 5rem;
+  `}
+
+  ${query.xl`
+    padding-left: 10rem;
+  `}
+`;
+
+export default function Form() {
+  return (
+    <FormContainer>
+      <FormWrapper>
+        <SectionHeading color="white" style={{ padding: '0 1rem' }} center={true}>
+          <FormattedMessage id="contact.form.header" />
+          <SubHeading color="silver">
+            <FormattedMessage id="contact.form.subheader" />
+          </SubHeading>
+        </SectionHeading>
+        <ContactForm />
+      </FormWrapper>
+      <SideBySideLadyImage />
+    </FormContainer>
+  );
+}
---
+++ src/views/Contact/MapControl.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+import { colors } from '../../util/styles';
+
+const ControlContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: translate(${props => props.x + 'px, ' + props.y + 'px'});
+  display: flex;
+  align-items: center;
+  color: ${colors.white};
+  cursor: pointer;
+`;
+
+const ControlCircle = styled.span`
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  border: 2px solid ${colors.white};
+  border-radius: 10px;
+  margin-right: 4px;
+  background: ${props => (props.active ? colors.white : 'transparent')};
+
+  ${ControlContainer}:hover & {
+    background: ${colors.white};
+  }
+`;
+
+export default function Control({ text, setActive, pos, active }) {
+  return (
+    <ControlContainer onClick={setActive} x={pos.x} y={pos.y}>
+      <ControlCircle active={active} />
+      {text}
+    </ControlContainer>
+  );
+}
